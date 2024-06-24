31c81c9510d9501d9688ed005968d4e700529c9a
PeterJLewis <peterj.lewis2010@gmail.com>
2024-06-24 11:10:30 +0100

    third commit

--- Setup.sh
+++ Setup.sh
@@ -8,6 +8,7 @@ sudo apt install gobuster -y
 sudo apt install feroxbuster -y
 sudo apt install bloodhound -y
 sudo apt install neovim -y
+wget https://github.com/PeterJLewis/KaliSetup/blob/main/init.vim -P ~/.config/nvim/
 sudo apt install snapd -y && sudo systemctl enable --now snapd apparmor && systemctl start --now snapd.apparmor && sudo snap install obsidian --classic && sudo apparmor_parser -r /etc/apparmor.d/*snap-confine* && sudo apparmor_parser -r /var/lib/snapd/apparmor/profiles/snap-confine* && sudo ln -s /snap/bin/obsidian /usr/bin/obsidian
 python3 -m pip install --user uploadserver
 sudo apt upgrade
