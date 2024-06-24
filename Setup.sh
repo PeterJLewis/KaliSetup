520234785e4a0bc3b281e91e5c6294c26e67c235
PeterJLewis <peterj.lewis2010@gmail.com>
2024-06-24 10:55:20 +0100

    second commit

---
+++ Setup.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/bash
+sudo apt update -y && sudo apt install exploitdb -y
+sudo apt install mingw-w64 -y
+sudo apt install npm -y
+sudo apt install peass -y
+sudo apt install chisel -y
+sudo apt install gobuster -y
+sudo apt install feroxbuster -y
+sudo apt install bloodhound -y
+sudo apt install neovim -y
+sudo apt install snapd -y && sudo systemctl enable --now snapd apparmor && systemctl start --now snapd.apparmor && sudo snap install obsidian --classic && sudo apparmor_parser -r /etc/apparmor.d/*snap-confine* && sudo apparmor_parser -r /var/lib/snapd/apparmor/profiles/snap-confine* && sudo ln -s /snap/bin/obsidian /usr/bin/obsidian
+python3 -m pip install --user uploadserver
+sudo apt upgrade
+wordlists
