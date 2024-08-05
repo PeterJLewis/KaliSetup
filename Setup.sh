bb085e8ed6606ac4f0b304bca6f2e993f132b4d8
PeterJLewis <102032656+PeterJLewis@users.noreply.github.com>
2024-08-05 09:37:18 +0100

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -9,6 +9,7 @@ sudo apt install gobuster -y
 sudo apt install feroxbuster -y
 sudo apt install bloodhound -y
 sudo apt install neovim -y
+sudo apt install zathura -y
 # Download and create init.vim for nvim
 wget https://raw.githubusercontent.com/PeterJLewis/KaliSetup/main/init.vim -P ~/.config/nvim/
 # Install snap and Obsidian
